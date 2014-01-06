d500afe5e300413640cef90846debc9dcc8f1fcf
Dai Wilson <moondwx@gmail.com>
2014-01-06 23:23:52 +0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name        "Wilson Dai"
+        email       "daiwx@live.com"
+        password    "foobar"
+        password_confirmation   "foobar"
+    end
+end
