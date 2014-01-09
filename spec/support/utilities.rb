7a5f4d924501971eb0697827f835cd7d705a60b2
Dai Wilson <moondwx@gmail.com>
2014-01-09 22:15:01 +0800

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -8,6 +8,17 @@ include ApplicationHelper
 #         "#{base_title} | #{page_title}"
 #     end
 # end
+def sign_in(user, options={})
+    if options[:no_capybara]
+        # Sign in when out using Capybara.
+        remember_token = User.new_remember_token
+        cookies[:remember_token] = remember_token
+        user.update_attribute(:remember_token, User.encrypt(remember_token))
+    else
+        visit signin_path
+        valid_signin(user)
+    end
+end
 
 def valid_signin(user)
     fill_in "Email",    with: user.email
